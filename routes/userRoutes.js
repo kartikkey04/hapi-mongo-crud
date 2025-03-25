71ea47dad60eca68618a08d992db748d66aa4f0c
kartikey <kartikkey0414@gmail.com>
2025-03-25 14:49:10 +0530

    getUser handler modified

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,7 @@ const userController = require('../controllers/userController');
 
 module.exports = [
     {method: 'POST', path: '/users', handler: userController.createUser},
-    {method: 'GET', path: '/users', handler: userController.getUser},
+    {method: 'GET', path: '/users', handler: userController.getUsers},
     {method: 'GET', path: '/users/{id}', handler: userController.getUserById},
     {method: 'PUT', path: '/users/{id}', handler: userController.updateUser},
     {method: 'DELETE', path: '/users/{id}', handler: userController.deleteUser}
