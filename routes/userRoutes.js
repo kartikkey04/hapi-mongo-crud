09164065ebf6dd6734473fd21a3e84b658e7d1cd
kartikey <kartikkey0414@gmail.com>
2025-03-26 10:15:29 +0530

    user routes redefined and validation added

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,52 @@
 const userController = require('../controllers/userController');
+const userSchema = require('../validations/userValidation');
 
 module.exports = [
-    {method: 'POST', path: '/users', handler: userController.createUser},
-    {method: 'GET', path: '/users', handler: userController.getUsers},
-    {method: 'GET', path: '/users/{id}', handler: userController.getUserById},
-    {method: 'PUT', path: '/users/{id}', handler: userController.updateUser},
-    {method: 'DELETE', path: '/users/{id}', handler: userController.deleteUser}
+    {
+        method: 'POST',
+        path: '/users',
+        options: {
+            validate: {
+                payload: userSchema.create,
+                failAction: (request, h, error) => error
+            }
+        },
+        handler: userController.createUser
+    },
+    {
+        method: 'GET',
+        path: '/users',
+        handler: userController.getUsers
+    },
+    {
+        method: 'GET',
+        path: '/users/{id}',
+        options: {
+            validate: {
+                params: userSchema.idParam
+            }
+        },
+        handler: userController.getUserById
+    },
+    {
+        method: 'PUT',
+        path: '/users/{id}',
+        options: {
+            validate: {
+                params: userSchema.idParam,
+                payload: userSchema.update
+            }
+        },
+        handler: userController.updateUser
+    },
+    {
+        method: 'DELETE',
+        path: '/users/{id}',
+        options: {
+            validate: {
+                params: userSchema.idParam
+            }
+        },
+        handler: userController.deleteUser
+    }
 ];
